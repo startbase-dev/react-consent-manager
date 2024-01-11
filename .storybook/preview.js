a0865b486b65c8737e80bb581eef7dd4f3f18062
Yunus ÖZCAN <yunusozcan@users.noreply.github.com>
2024-01-11 14:00:24 +0300

    Initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import '../src/style/globals.css';
+import './storybook.css';
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
